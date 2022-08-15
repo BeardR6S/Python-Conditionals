8bd0542ad3d6bbabe24922e20af72e5c3ee087f5
BeardR6S <brandon.allan.hale@gmail.com>
2022-08-14 23:23:40 -0400

    send it

--- UseTernaryOpInPythonCondit.py
+++ UseTernaryOpInPythonCondit.py
@@ -2,7 +2,7 @@ role = 'Admin'
 
 auth = 'can access' if role == 'Admin' else 'cannot access'
 
-print(auth) #this and everything above it = ternary operator
+print(auth) #this and everything above it == ternary operator
 
 # if role == 'Admin':
 #     auth = 'can access'
